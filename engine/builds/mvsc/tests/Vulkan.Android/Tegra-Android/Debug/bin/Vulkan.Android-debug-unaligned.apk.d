6699eaf20416e0a951b730fd3d673f243bf02d3b
Pooya Eimandar <pooya.eimandar@live.com>
2017-02-26 13:07:42 -0800

    version 1.0.4.0
    version 1.0.4.0

---
+++ engine/builds/mvsc/tests/Vulkan.Android/Tegra-Android/Debug/bin/Vulkan.Android-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+E:\SourceCode\CG\Wolf.Engine\engine\builds\mvsc\Tests\Vulkan.Android\Tegra-Android\Debug\bin\Vulkan.Android-debug-unaligned.apk : \
+E:\SourceCode\CG\Wolf.Engine\engine\builds\mvsc\Tests\Vulkan.Android\Tegra-Android\Debug\bin\Vulkan.Android.ap_ \
+E:\SourceCode\CG\Wolf.Engine\engine\builds\mvsc\Tests\Vulkan.Android\Tegra-Android\Debug\libs\armeabi-v7a\gdbserver \
+E:\SourceCode\CG\Wolf.Engine\engine\builds\mvsc\Tests\Vulkan.Android\Tegra-Android\Debug\libs\armeabi-v7a\libVulkan.Android.so \
---
+++ engine/builds/mvsc/tests/Vulkan.Android/Tegra-Android/Debug/bin/Vulkan.Android.ap_.d
@@ -0,0 +1,4 @@
+E:\SourceCode\CG\Wolf.Engine\engine\builds\mvsc\Tests\Vulkan.Android\Tegra-Android\Debug\bin\Vulkan.Android.ap_ \
+ : E:\SourceCode\CG\Wolf.Engine\engine\builds\mvsc\Tests\Vulkan.Android\res\values\strings.xml \
+E:\SourceCode\CG\Wolf.Engine\engine\builds\mvsc\Tests\Vulkan.Android\assets\test.txt \
+E:\SourceCode\CG\Wolf.Engine\engine\builds\mvsc\Tests\Vulkan.Android\Tegra-Android\Debug\bin\AndroidManifest.xml \
