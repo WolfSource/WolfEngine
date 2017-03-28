79a65e3732947d4bf942e1d1e4309e982b1e964e
Pooya Eimandar <pooya.eimandar@live.com>
2017-03-28 16:58:20 +0430

    v.1.2.11.0 iOS just added

--- engine/src/tests/vulkan.iOS/AppDelegate.m
+++ engine/src/tests/vulkan.iOS/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#include "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -51,6 +52,7 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    [ViewController Release];
 }
 
 
