51e8ec299ffbc2848f7bb98b2e2359b54fde093e
Pooya Eimandar <pooya.eimandar@live.com>
2017-08-09 19:09:40 +0430

    v.1.30.3.11

--- engine/src/tests/scene.cpp
+++ engine/src/tests/scene.cpp
@@ -340,8 +340,9 @@ HRESULT scene::_load_areas()
         const std::vector<std::wstring> _areas_on_thread_0 =
         {
             L"models/model.dae"
+            //L"models/_120_water-treatment_v1_16_19.dae",
+
             /*L"models/_171_173_office_building_comprehensive_v1_14_13.dae",
-            L"models/_120_water-treatment_v1_16_19.dae",
             L"models/_161_air-compressor-v1_31_16.dae",
             L"models/_151_substation_v_1_40_17.dae"*/
         };
