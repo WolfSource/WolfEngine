8ddf2dd0856d3918c92d628922057a1cd354d015
Pooya Eimandar <pooya.eimandar@live.com>
2018-03-15 06:24:10 +0330

    v.1.60.17.26

--- install.sh
+++ install.sh
@@ -31,9 +31,11 @@ for i in "${!keys[@]}"; do
    fi
 done
 
+cp -R "./vulkan.framework" "./vulkan/Mac/macOS/Frameworks/"
+
 echo "start building Wolf"
 case "$OSTYPE" in
-  darwin*)  cp -R "./vulkan.framework" "./vulkan/Mac/macOS/Frameworks/" ;; xcodebuild clean build -workspace ../../engine/builds/xcode/wolf.engine.vulkan.macOS.xcworkspace -scheme test_vulkan_macOS -sdk macosx10.13 -configuration Debug ;; 
+  darwin*)  xcodebuild clean build -workspace ../../engine/builds/xcode/wolf.engine.vulkan.macOS.xcworkspace -scheme test_vulkan_macOS -sdk macosx10.13 -configuration Debug ;; 
   linux*)   echo "LINUX" ;;
   *)        echo "OS: $OSTYPE" ;;
 esac
