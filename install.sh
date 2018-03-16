e3fa00760fa78bc188907b35a86efdd0d1a06f61
Pooya Eimandar <pooya.eimandar@live.com>
2018-03-17 01:33:21 +0330

    v.1.60.18.1

--- install.sh
+++ install.sh
@@ -35,7 +35,10 @@ tar -zxvf ./vulkan/macOS.tar.gz -C ./vulkan/
 
 echo "start building Wolf"
 case "$OSTYPE" in
-  darwin*)  xcodebuild clean build -workspace ../../engine/builds/xcode/wolf.engine.vulkan.macOS.xcworkspace -scheme test_vulkan_macOS -sdk macosx10.13 -configuration Debug ;; 
+  darwin*)  
+            xcodebuild clean build -workspace ../../engine/builds/xcode/wolf.engine.vulkan.macOS.xcworkspace -scheme test_vulkan_macOS -sdk macosx10.13 -configuration Debug 
+            xcodebuild clean build -workspace ../../engine/builds/xcode/pyWolf.xcworkspace -scheme pyWolf.system -sdk macosx10.13 -configuration Debug 
+            ;; 
   linux*)   echo "LINUX" ;;
   *)        echo "OS: $OSTYPE" ;;
 esac
