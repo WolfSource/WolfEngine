76cf88a03ee82de4e4768b24de58b4e320666323
foroughmajidi <51900859+foroughmajidi@users.noreply.github.com>
2020-08-11 18:39:18 +0430

    Update test.net.c

--- engine/tests/test.net.c
+++ engine/tests/test.net.c
@@ -1,5 +1,7 @@
 #include <wolf.h>
-#include<net/w_net.h>
+#include <net/w_net.h>
+
+
 
 int main() {
 	wolf_initialize();
@@ -16,48 +18,51 @@ int main() {
 	const char pPort[] = "5555";
 	w_socket_address pSocketAddress = (w_socket_address)w_malloc(sizeof(w_socket_address), "");
 	int pBindOrConnect = 0;
-	W_RESULT _net_resolve = w_net_resolve(pURL, pPort, IPV4, 1, pSocketAddress);
+	W_RESULT _net_resolve = w_net_resolve(pURL, pPort, IPV4, 0, pSocketAddress);
 
 	udp_socket.sa = (w_socket_address)w_malloc(sizeof(w_socket_address), "");
 	udp_socket.a = (w_aio)w_malloc(sizeof(w_aio), "");
 	udp_socket.u = (w_udp_protocol)w_malloc(sizeof(w_udp_protocol), "");
 
-	char* pMessage_udp = "yes";
-	size_t pMessageLength_udp = strlen(pMessage_udp);
+	
 	const char* endpoint = "udp://127.0.0.1:5555";
 	W_RESULT net_open_udp_socket = w_net_open_udp_socket(endpoint, &udp_socket);
-
-	const char* pEndPoint_1 = "tcp://127.0.0.1:5555";
-	const char* pTLSServerName = "127.0.0.1";
+	
+	const char* pEndPoint_1 = "tcp://127.0.0.1:5554";
+	
 	w_socket_tcp pSocket;
 	pSocket.s = (w_socket)w_malloc(sizeof(w_socket), "");
 	pSocket.l = (w_listener)w_malloc(sizeof(w_listener), "");
 	pSocket.d = (w_dialer)w_malloc(sizeof(w_dialer), "");
 
 	W_RESULT net_open_tcp_socket = w_net_open_tcp_socket(pEndPoint_1,
-		req_rep_listener, false,
-		false, false,
+		two_way_dialer, false,
+		false, true,
 		false,
 		0,
-		pTLSServerName,
+		NULL,
 		false,
-		"",
-		"",
+		NULL,
+		NULL,
 		&pSocket);
 
+	    int request_nbr=0;
 
-	char pMessage[] = "yes";
-	size_t pMessageLength = strlen(pMessage);
-	W_RESULT _net_send_msg_tcp = w_net_send_msg_tcp(&pSocket, pMessage, pMessageLength, true);
+	    char buffer[10];
+		char buf_sender_message[256];
+		const char* s = "|";
+		const char* s1 = "ping";
+		snprintf(buf_sender_message, sizeof buf_sender_message, "%d%s%s", request_nbr, s, s1);
 
-	W_RESULT _net_close_tcp_socket = w_net_close_tcp_socket(&pSocket);
+		const char* sender_message = buf_sender_message;
+		void* mm = (void*)sender_message;
+		
+   W_RESULT _net_send_msg_tcp = w_net_send_msg_tcp(&pSocket, mm, strlen(mm), false);
+	
+   W_RESULT _net_close_tcp_socket = w_net_close_tcp_socket(&pSocket);
+	
 
 
-	const char* _net_erro = w_net_error(_net_send_msg_tcp);
-
-
-
-
-	wolf_terminate();
-	return 0;
+   wolf_terminate();
+   return 0;
 }
