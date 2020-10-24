2ac982c6face1f5fe414eb4d0769df29194074f7
forough majidi <forough_majidi@yahoo.com>
2020-10-24 17:48:18 +0330

    add activemq

---
+++ engine/src/wolf.system/net/amq/amq_config.h
@@ -0,0 +1,21 @@
+/*
+	Project			 : ActiveMQ-APIs
+	Source			 : Please direct any bug to https://github.com/FanapSoft/ActiveMQ-APIs/issues
+	Website			 : http://FanapSoft.github.io
+	Name			 : amq_config.h
+	Description		 : configuration for your broker  
+	Comment          :
+*/
+
+#ifndef __AMG_PRODUCER_H__
+#define __AMG_PRODUCER_H__
+
+#define BROKER_URI              "url"
+#define USER_NAME               "user"
+#define PASS_WORD               "pass"
+#define PRODUCER_QUEUE_NAME     "producer_queue_name"
+#define CONSUMER_QUEUE_NAME     "consumer_queue_name"
+#define PRODUCER_TOPIC_NAME     "producer_topic_name"
+#define CONSUMER_TOPIC_NAME     "consumer_topic_name"
+
+#endif
