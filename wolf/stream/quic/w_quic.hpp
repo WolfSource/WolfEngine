f3f1e239773b6da7154b5891e7b894185e8af801
PooyaEimandar <pooya.eimandar@gmail.com>
2022-08-04 00:05:57 +0430

    build: quic

---
+++ wolf/stream/quic/w_quic.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <wolf.hpp>
+
+namespace wolf::stream::quic
+{
+  class w_quic
+  {
+  public:
+    w_quic();
+    virtual ~w_quic(){};
+
+  private:
+  };
+} // namespace wolf::stream::http
